6216f47a2c604c5f02333e964f33db285ac53462
Soumen sinha mahapatra <soumen1696@gmail.com>
2018-11-15 16:09:23 +0530

    Homepage

---
+++ Homepage/home.php
@@ -0,0 +1,141 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <!--favicon-->
+        <link rel="shortcut icon" href="favicon.ico" type="image/icon">
+        <link rel="icon" href="favicon.ico" type="image/icon">
+        <title>CUSAT Home</title>
+        <!-- Bootstrap Core CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+        <!-- Footer -->
+        <link type="text/css" rel="stylesheet" href="css/style.css">
+        <!-- Custom Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800'
+        rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic'
+        rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css" type="text/css">
+        <!-- Plugin CSS -->
+        <link rel="stylesheet" href="css/animate.min.css" type="text/css">
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="css/creative.css" type="text/css">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body id="page-top">
+
+        <nav id="mainNav" class="navbar navbar-default navbar-fixed-top" style="background:black">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="http://www.cusat.ac.in">Cochin University Of Science And  Technology</a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a class="page-scroll" href="../Profilers/SProfile/index.php">Student Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../Profilers/HODProfile/index.php">HOD Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../Profilers/PProfile/index.php">Placement Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="../Profilers/PriProfile/index.php">Administrative Login</a>
+                        </li>
+                    </ul>
+
+            </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+        <header>
+            <div class="header-content">
+                <div class="header-content-inner">
+                    <h1>PLACEMENT MANAGEMENT SYSTEM</h1>
+                    <hr>
+                    <p>We are here to Build your Skills and Career with our Driven Passion and Reality.</br>Click Below to Get Our Current Drive
+                        Details</p>
+                    <a href="../Drives/index.php" class="btn btn-primary btn-xl page-scroll">Current Drives</a>
+                </div>
+            </div>
+        </header>
+        <div class="footer">
+			<div class="container">
+				<div class="col-md-3 ftr_navi ftr">
+					<h3>NAVIGATION</h3>
+					<ul>
+						<li>
+							<a href="../Homepage/index.php">Home</a>
+						</li>
+						<li>
+							<a href="../Profilers/SProfile/index.php">Student Login</a>
+						</li>
+						<li>
+							<a href="../Profilers/PProfile/index.php">Placement Login</a>
+						</li>
+						<li>
+							<a href="../Profilers/HODProfile/index.php">HOD Login</a>
+						</li>
+						<li>
+							<a href="../Profilers/Priprofile/index.php">Administrative Login</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-3 ftr_navi ftr">
+					<h3>MEMBERS</h3>
+					<ul>
+						<li>
+							<a href="#">Customer Support</a>
+						</li>
+						<li>
+							<a href="#">Placement Support</a>
+						</li>
+						<li>
+							<a href="#">Faculty Support</a>
+						</li>
+						<li>
+							<a href="#">Registered Companies</a>
+						</li>
+						<li>
+							<a href="#">Training</a>
+						</li>
+					</ul>
+				</div>
+				<div class="col-md-3 get_in_touch ftr">
+					<h3>GET IN TOUCH</h3>
+					<p>University Hills</p>
+					<p>Kalamessery, Kochi, India</p>
+					<p>83019-79790/221456</p>
+					<a href="mailto:soumen1696@gmail.com">Soumen1696@gmail.com</a>
+				</div>
+				<div class="col-md-3 ftr-logo">
+					<p>Copyright &copy; 2018 CUSAT-SOE | Developed by
+              <a href="http://znumerique.azurewebsites.net" target="_parent">HyperMine Technologies</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+    </body>
+
+</html>
---
+++ Homepage/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv='refresh' content ='3; url=home.php'>
+		</head>
+		<body>
+<div style="
+    height: 200px;
+    width: 400px;
+
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    margin-top: -100px;
+    margin-left: -200px;">
+	<img src="images/wds.png"/>
+	<h1> Welcome to CUSAT</h1>
+	<h3> Advancing Placements </h3>
+	<h4> Please Wait While we Redirect you to our Page </h4>
+	<h6> Not Redirected?? <a href="home.php">Click Here</a></h6>
+	</div>
+	</body>
+	</html>
